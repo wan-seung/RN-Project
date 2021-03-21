5de7a63938526bd0e94d200c511570c8484a9629
김승완 <reddht721@gmail.comlspwdcdgit config --global user.email reddht721@gmail.com>
2021-03-21 23:31:08 +0900

    WIP

--- App.js
+++ App.js
@@ -12,59 +12,26 @@ import {
   SafeAreaView,
 } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function App() {
   return (
     <NavigationContainer>
-      <SafeAreaView style={styles.container}>
-        <View style={styles.profileContainer}>
-          {/* <Image style={style.image} source={{uri: }}/> */}
-          <Image style={styles.image} source={require('./assets/wanie.jpeg')} />
-          <View>
-            <TouchableOpacity activeOpacity={0.3}>
-              <Text>user name : </Text>
-            </TouchableOpacity>
-          </View>
-          <TextInput style={styles.textInput} />
-        </View>
-        <View style={styles.profileContainer}>
-          <Image style={styles.image} source={require('./assets/you.png')} />
-          <View>
-            <TouchableOpacity activeOpacity={0.3}>
-              <Text>user name : </Text>
-            </TouchableOpacity>
-          </View>
-          <TextInput style={styles.textInput} />
-        </View>
-      </SafeAreaView>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
-{
-  /* <StatusBar style="light" /> */
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  profileContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  image: {
-    width: 100,
-    height: 100,
-    marginRight: 10,
-    marginTop: 10,
-  },
-  textInput: {
-    width: 100,
-    height: 50,
-    borderWidth: 1,
-  },
-});
+export default App;
---
+++ Wanie.js
@@ -0,0 +1,55 @@
+export default function App() {
+  return (
+    <NavigationContainer>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.profileContainer}>
+          {/* <Image style={style.image} source={{uri: }}/> */}
+          <Image style={styles.image} source={require('./assets/wanie.jpeg')} />
+          <View>
+            <TouchableOpacity activeOpacity={0.3}>
+              <Text>user name : </Text>
+            </TouchableOpacity>
+          </View>
+          <TextInput style={styles.textInput} />
+        </View>
+        <View style={styles.profileContainer}>
+          <Image style={styles.image} source={require('./assets/you.png')} />
+          <View>
+            <TouchableOpacity activeOpacity={0.3}>
+              <Text>user name : </Text>
+            </TouchableOpacity>
+          </View>
+          <TextInput style={styles.textInput} />
+        </View>
+      </SafeAreaView>
+    </NavigationContainer>
+  );
+}
+
+{
+  /* <StatusBar style="light" /> */
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  profileContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  image: {
+    width: 100,
+    height: 100,
+    marginRight: 10,
+    marginTop: 10,
+  },
+  textInput: {
+    width: 100,
+    height: 50,
+    borderWidth: 1,
+  },
+});
